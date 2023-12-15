f84d80d78a7ef84fc8a61e3df1812aa604b19777
Kir_Antipov <kp.antipov@gmail.com>
2023-12-15 17:17:44 +0300

    Ported the mod to 1.20.x

--- src/main/java/dev/kir/grindstoneismadeofstone/mixin/GrindstoneBlockMixin.java
+++ src/main/java/dev/kir/grindstoneismadeofstone/mixin/GrindstoneBlockMixin.java
@@ -2,8 +2,7 @@ package dev.kir.grindstoneismadeofstone.mixin;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.GrindstoneBlock;
-import net.minecraft.block.MapColor;
-import net.minecraft.block.Material;
+import net.minecraft.block.piston.PistonBehavior;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -11,11 +10,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(GrindstoneBlock.class)
 public class GrindstoneBlockMixin {
     @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/WallMountedBlock;<init>(Lnet/minecraft/block/AbstractBlock$Settings;)V"))
-    private static AbstractBlock.Settings changeSettingsMaterialToStone(AbstractBlock.Settings settings) {
-        Material material = Material.STONE;
-        MapColor color = material.getColor();
-        settings.material = material;
-        settings.mapColorProvider = state -> color;
-        return settings;
+    private static AbstractBlock.Settings changePistonBehaviorToNormal(AbstractBlock.Settings settings) {
+        return settings.pistonBehavior(PistonBehavior.NORMAL);
     }
 }
